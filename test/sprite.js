90a1c92fccb688130e3d9b64c83f36d8f49b279b
Damian Krzeminski <pirxpilot@furkot.com>
2023-03-26 21:34:09 +0200

    add sprite render implementation

--- test/sprite.js
+++ test/sprite.js
@@ -1,8 +1,49 @@
-const test = require('tape');
-const mapwhitSprite = require('..');
+const assert = require('node:assert');
+const { readFile } = require('node:fs/promises');
+const path = require('node:path');
+const test = require('node:test');
+const { render } = require('../lib/sprite');
 
-test('mapwhit-sprite must have at least one test', function (t) {
-  mapwhitSprite();
-  t.fail('Need to write tests.');
-  t.end();
+test('empty', async function () {
+  const layout = {
+    dim: { width: 1, height: 1 },
+    layout: {}
+  };
+  const result = await render([], layout);
+  assert.ok(Buffer.isBuffer(result));
+});
+
+test('simple', async function () {
+
+  const images = [
+    {
+      id: 'r',
+      svg: `
+        <svg width="50" height="40">
+          <rect width="25" height="35" x="5" y="5" fill="green" />
+        </svg>
+      `
+    },
+    {
+      id: 'c',
+      svg: `
+        <svg width="50" height="40">
+          <circle cx="20" cy="15" r="12" fill="red" />
+        </svg>
+      `
+    }
+  ].map(({ id, svg }) => ({ id, img: Buffer.from(svg) }));
+
+  const layout = {
+    dim: { width: 100, height: 100 },
+    layout: {
+      r: { x: 10, y: 20 },
+      c: { x: 50, y: 40 }
+    }
+  };
+
+  const result = await render(images, layout);
+  const expected = await readFile(path.resolve(__dirname, 'fixtures/simple.png'));
+  assert.ok(Buffer.isBuffer(result));
+  assert.equal(Buffer.compare(result, expected), 0, 'should be equal to reference image');
 });
