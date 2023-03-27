aaea77ea86adebca06debb458e972c18040a2920
Damian Krzeminski <pirxpilot@furkot.com>
2023-03-27 08:09:55 +0200

    export `grid` and `pack` convenience functions

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const sprite = require('./lib/sprite');
+const layout = require('./lib/layout');
+
 module.exports = {
-  sprite: require('./lib/sprite')
+  sprite,
+  layout,
+  pack: makePack,
+  grid: makeGrid
 };
+
+async function makePack(imgs) {
+  const pack = layout.pack(imgs);
+  return {
+    sprite: await sprite.render(imgs, pack),
+    layout: pack
+  };
+}
+
+async function makeGrid(imgs, params) {
+  const grid = layout.grid(imgs, params);
+  return {
+    sprite: await sprite.render(imgs, grid),
+    layout: grid
+  };
+}
